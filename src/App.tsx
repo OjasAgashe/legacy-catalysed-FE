584aac526f88577fc8c4128176d7d555f2c89e66
Md.Mainul Islam <u1902141@student.cuet.ac.bd>
2021-02-02 15:12:15 +0600

    typescript added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import OrganizationRegistration from './components/OrganizationRegistration/OrganizationRegistration';
-
-
 function App() {
   return (
     <div className="App">
-     <OrganizationRegistration/>
+      <Router>
+        <Route path="/home">
+          <OrganizationRegistration/>
+        </Route>
+        <Route exact path="/">
+       <OrganizationRegistration/>
+        </Route>
+      </Router>
+  
     </div>
   );
 }
--- src/components/OrganizationRegistration/OrganizationRegistration.tsx
+++ src/components/OrganizationRegistration/OrganizationRegistration.tsx
@@ -5,7 +5,7 @@ const OrganizationRegistration = () => {
     return (
         <>
          <div  className="d-flex justify-content-center registration-mother-container " >
-            <div className="registration-container container my-auto bg-white">
+            <div className="registration-container container-fluid  my-auto bg-white">
             <h1>Create an account</h1> 
         <form className=" container  mt-4" >
             <div className="d-flex ">
@@ -15,7 +15,7 @@ const OrganizationRegistration = () => {
             <input type="email" className="form-control form-control-sm " placeholder="Your Official Email Id" required/><br/>
             <input type="password" className="form-control form-control-sm " placeholder="Password" required/><br/>
             <input type="password"className="form-control form-control-sm " placeholder="Confirm Password" required />
-            <input type="submit" className="btn btn-danger mt-4 w-100" value="Register now"/>
+            <input type="submit" className="btn btn-success mt-4 w-100" value="Register now"/>
   
         </form>
         <p className="mt-3">Do you have an account?  <span  className="text-primary login-click"><u>Click here to login</u></span> </p>
