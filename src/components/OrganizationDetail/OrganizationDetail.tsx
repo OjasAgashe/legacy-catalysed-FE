b8290fa9f4923182ab0a97461307c3133eaf8d67
Md.Mainul Islam <u1902141@student.cuet.ac.bd>
2021-02-06 22:37:21 +0600

    done as my own perspective and added typescript

--- src/components/OrganizationDetail/OrganizationDetail.tsx
+++ src/components/OrganizationDetail/OrganizationDetail.tsx
@@ -8,7 +8,7 @@ const OrganizationDetail = () => {
             <div className="organization-registration-container container-fluid  my-auto bg-white">
            <input type="text" className="form-control mt-2 organization-detail-input form-control-sm first-name " placeholder="Organization name"/>
             <textarea name="" className="form-control mt-3 mb-3 organization-detail-textarea" id="" cols={5} rows={5}  placeholder="Organization detail *"/>
-            <h6><u>Guidelines</u></h6>
+            <h3><u>Guidelines</u></h3>
           
        
       
