f850cc1c9fff7f4e70b5df60a65ead3bd270650c
Md.Mainul Islam <u1902141@student.cuet.ac.bd>
2021-02-09 08:53:14 +0600

    first name alignment and organization page done

--- src/components/OrganizationRegistration/OrganizationRegistration.tsx
+++ src/components/OrganizationRegistration/OrganizationRegistration.tsx
@@ -14,7 +14,7 @@ const OrganizationRegistration = () => {
          <div  className="d-flex justify-content-center organization-registration-mother-container " >
             <div className="organization-registration-container container-fluid  my-auto bg-white">
             <h1>Create an account</h1> 
-        <form className=" container  mt-4" >
+        <form onSubmit={onGoingNext}  className=" container  mt-4" >
             <div className="d-flex   ">
             <input type="text" className="form-control form-control-sm first-name overflow-hidden " placeholder="First Name"required/> &nbsp;<br/>
             <input type="text" className="form-control form-control-sm last-name  overflow-hidden " placeholder="Last Name"required/>
@@ -22,7 +22,7 @@ const OrganizationRegistration = () => {
             <input type="email" className="form-control form-control-sm " placeholder="Your Official Email Id"required/><br/>
             <input type="password" className="form-control form-control-sm " placeholder="Password"required/><br/>
             <input type="password"className="form-control form-control-sm " placeholder="Confirm Password"required />
-            <input type="submit" onClick={onGoingNext} className="btn btn-success mt-4 w-100" value="Register now"/>
+            <input type="submit" className="btn btn-success mt-4 w-100" value="Register now"/>
   
         </form>
         <p className="mt-3">Do you have an account?  <span  className="text-primary login-click"><u>Click here to login</u></span> </p>
