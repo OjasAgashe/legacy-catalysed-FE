532b02653ca6b3e67c5d0f7521c3d397dee422d2
Md.Mainul Islam <u1902141@student.cuet.ac.bd>
2021-02-15 01:39:12 +0600

    changes done

--- src/components/HomePageContainer/AboutUs/AboutUs.tsx
+++ src/components/HomePageContainer/AboutUs/AboutUs.tsx
@@ -5,17 +5,17 @@ import boy from'../../../images/boy.png'
 const AboutUs = () => {
     return (
         <>
-        <div className="row w-100">
+        <div className="row w-auto mw-auto p-0">
             <div className="col-md-3  p-0">
-            <img src={girl} className=" w-md-100 girl" />
+            <img src={girl} className=" w-100 h-100 girl" />
             </div>
-            <div className="col-md-6 w-md-100 about-us-background">
-             <h1 className="about-us-title w-md-auto">About <span className="us">Us</span> </h1>
+            <div className="col-md-6 mw-auto about-us-background">
+             <h1 className="about-us-title ">About <span className="us">Us</span> </h1>
              <h4 className="about-us-title-second mt-4">Give every child's dream a better chance</h4>
              <p className="about-us-description text-justify mt-3">We aim to help every child colour the world by facilitating easy and streamlined access to quality guidance and education by providing them with a platform that helps them unveil their potential and introduce them to phenomenal and unforeseen opportunities.</p>
             </div>
-            <div className="col-md-3  p-0">
-          <img src={boy} className="w-md-100 boy " />
+            <div className="col-md-3 p-0">
+          <img src={boy} className="w-100 h-100 boy " />
             </div>
         </div>
             
