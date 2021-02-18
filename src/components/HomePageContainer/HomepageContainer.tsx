c85145a51588e45388078e12def5338bde69c9b8
Md.Mainul Islam <u1902141@student.cuet.ac.bd>
2021-02-18 23:08:54 +0600

    text alignment of each div done

--- src/components/HomePageContainer/HomepageContainer.tsx
+++ src/components/HomePageContainer/HomepageContainer.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 
 import './HomePageContainer.css'
-
-
-
 import girl from '../../images/girl.png';
 import boy from '../../images/boy.png';
 import rabbit from '../../images/rabbit.png';
@@ -61,11 +58,11 @@ const HomepageContainer = () => {
             <div className="col-md-4  p-0">
                 <img src={rabbit} className="w-100 h-100 p-0"/>
             </div>
-            <div className="col-md-8   personalized-profile-background   justify-content-center ">
-            <div className="mt-5">
-                <h2 className=" personalized-profile-title mt-2 text-center">Personalized Profile <br/> Builder</h2>
+            <div className="col-md-8 align-items-center d-flex personalized-profile-background   justify-content-center ">
+            <div className="">
+                <h2 className=" personalized-profile-title  text-center">Personalized Profile <br/> Builder</h2>
           
-                <p className="personalized-profile-description text-center mb-5 fw-normal mt-5">
+                <p className="personalized-profile-description text-center text-justify fw-normal mt-5">
                 Our personalized profile builder is filled with amusing<br/> questions and queries to gauge your interests and <br/> hobbies. Helping us in this is our fine-tuned NLP<br/> engine to identify and decode features that don't meet<br/> the eye. Privacy of data is one of our main concerns <br/>and we tackle that through homomorphic encryption, <br/>eliminating the scenario of unsecured data.
             
                 </p>
@@ -75,10 +72,10 @@ const HomepageContainer = () => {
         {/* <PersonalizedProfile/> */}
           {/* <SearchRecommendation/> */}
           <div className=" row w-100 ">
-                <div className="col-md-8 search-recommendation-background">
-                    <div className="mt-5">
+                <div className="col-md-8 d-flex justify-content-center align-items-center search-recommendation-background">
+                    <div className="">
                     <h2 className="search-recommendation-title d-flex justify-content-center container">Search and<br/> Recommendation</h2>
-                   <p className="mt-5 search-recommendation-description text-center">The student can search for mentors and show intent to connect.<br/> The search is made seamless assisted by Elasticsearch. The<br/> recommendation system powered by a hybrid of collaborative <br/>and content based filtering uses the profile questions to match<br/> similar students and mentors.
+                   <p className="search-recommendation-description text-center text-justify mt-5">The student can search for mentors and show intent to connect.<br/> The search is made seamless assisted by Elasticsearch. The<br/> recommendation system powered by a hybrid of collaborative <br/>and content based filtering uses the profile questions to match<br/> similar students and mentors.
                    </p>
                     </div>
                   
@@ -93,10 +90,10 @@ const HomepageContainer = () => {
                 <div className="col-md-4 p-0">
                     <img src={building} className="w-100 h-100 "/>
                 </div>
-                <div className="col-md-8 programs-interest-background">
-                    <div className="mt-5">
+                <div className="col-md-8 d-flex justify-content-center align-items-center programs-interest-background">
+                    <div className="">
                     <h2 className="programs-interest-title">Programs and <br/>Interest Groups</h2>
-                    <p className=" mt-5 mb-2 programs-interest-description my-auto mb-5">The platform quickly learns your interests and<br/> preference and introduces to you to programs and <br/>interest groups best for you. Our collaboration with<br/> multiple not-for-profit organizations and accomplished <br/>mentors enable us to pioneer fellowships that provide <br/>students with leadership development, cross-cultural<br/> experience and the opportunity to make a progressive<br/> social impact not only on themselves but the world at<br/> large.</p>
+                    <p className="  programs-interest-description my-auto mt-5">The platform quickly learns your interests and<br/> preference and introduces to you to programs and <br/>interest groups best for you. Our collaboration with<br/> multiple not-for-profit organizations and accomplished <br/>mentors enable us to pioneer fellowships that provide <br/>students with leadership development, cross-cultural<br/> experience and the opportunity to make a progressive<br/> social impact not only on themselves but the world at<br/> large.</p>
                     </div>
                    
                 </div>
@@ -104,8 +101,8 @@ const HomepageContainer = () => {
              {/* <ProgramsInterest/> */}
           {/* <PlanSchedule/> */}
           <div className="row w-100">
-                <div className="col-md-8 plan-schedule-background">
-                    <div className="mt-5">
+                <div className="col-md-8 plan-schedule-background d-flex justify-content-center align-items-center">
+                    <div className="">
                    <h1 className="plan-schedule-title">Plan, Schedule and<br/> Document</h1>
                    <p  className="mt-5 plan-schedule-description mb-5">Our stack behind the platform makes it effortless for all<br/> users involved to carry on with their communication without<br/> worrying about the nitty-gritty of it. We offer a range of built<br/> in tools to plan and keep track of the valued interactions <br/>taking place on the platform, as well as the option to <br/>continue them on third-party services outside the platform. <br/>Special nudges designed using behavioural architecture<br/> principles are blended into the interface assisting every <br/>student at each step to achieve his/her goals.</p>
                    </div>
